8c8dc2aaeffdd276c5ee750b32829797a92478b0
Artur Taraszka <kret335@gmail.com>
2021-04-27 11:57:12 +0200

    ATRecipeList

---
+++ src/main/java/pl/coderslab/web/RecipieList.java
@@ -0,0 +1,33 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.RecipieDao;
+import pl.coderslab.model.Recipie;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "RecipieList", value = "/app/recipe/list")
+public class RecipieList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        RecipieDao recipieDao = new RecipieDao();
+
+        List<Recipie> list = recipieDao.showAll();
+        response.getWriter().println(list.get(1).getName());
+        session.setAttribute("list", list);
+
+        request.setCharacterEncoding("UTF-8");
+
+        getServletContext().getRequestDispatcher("/app/recipe/RecipieList.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
